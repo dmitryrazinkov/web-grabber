351ea1187ad111cdb26221196f39f4d0e7f1bd88
quiet_di <razinkov_dmitrij@mail.ru>
2014-12-29 00:29:56 +0300

    open tab with start app

--- src/main/java/grub/app/Launcher.java
+++ src/main/java/grub/app/Launcher.java
@@ -3,6 +3,8 @@ package grub.app;
 import org.springframework.boot.SpringApplication;
 
 import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
 
 public class Launcher {
     public static void main(final String[] args) {
@@ -15,6 +17,14 @@ public class Launcher {
                 //   jFrame.setLocation(450, 150);
                 //   jFrame.setSize(500, 400);
                 //   jFrame.setVisible(true);
+                Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+                if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+                    try {
+                        desktop.browse(new URL("http://localhost:4040").toURI());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
 
             }
         });
