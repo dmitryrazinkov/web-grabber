e7713d42ce696e9ec04c174cb4736df3a1c56c54
quiet_di <razinkov_dmitrij@mail.ru>
2015-01-24 23:16:04 +0300

    harvest accesor simple+ all dependesy add

--- src/main/java/grub/withHarvest/HarvestAccessor.java
+++ src/main/java/grub/withHarvest/HarvestAccessor.java
@@ -1,5 +1,22 @@
 package grub.withHarvest;
 
+import org.webharvest.definition.ScraperConfiguration;
+import org.webharvest.runtime.Scraper;
+
+import java.io.FileNotFoundException;
+
 public class HarvestAccessor {
+    public static void main(String[] args) {
+        try {
+            ScraperConfiguration config=new ScraperConfiguration("C:\\Users\\Дмитрий\\Desktop\\Новая папка\\test.xml");
+            Scraper scraper=new Scraper(config,"C:\\");
+            scraper.setDebug(true);
+            scraper.execute();
+            System.out.println(scraper.getContext().getVar("out").toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
