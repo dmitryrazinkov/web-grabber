b92588c157c1d150814c56933a8bed9f6e8254b6
quiet_di <razinkov_dmitrij@mail.ru>
2015-02-10 02:22:15 +0300

    /

--- src/main/java/grub/Strategy/OnChangeStrategy.java
+++ src/main/java/grub/Strategy/OnChangeStrategy.java
@@ -1,18 +1,4 @@
-package grub.strategy;
+package grub.Strategy;
 
-import grub.entities.GrubResult;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-@Component
 public class OnChangeStrategy {
-    private static final Logger log = LoggerFactory.getLogger(OnChangeStrategy.class);
-
-    public boolean isChanged(GrubResult one, GrubResult two) {
-        if (one.getStringScriptOutput().getStringResult().equals(two.getStringScriptOutput().getStringResult())) {
-            return false;
-        } else return true;
-    }
-
 }
